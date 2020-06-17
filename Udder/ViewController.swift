120b4227ddda66bbce8219a07cb38a0f10e8dd9d
Uday <UD341583@wipro.com>
2020-06-17 18:15:20 +0530

    initial commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Uday Kumar Abhishek . (Service Transformation) on 17/06/20.
+//  Copyright © 2020 Uday Kumar Abhishek . (Service Transformation). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
